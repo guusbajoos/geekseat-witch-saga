9dc65de906f652238c45c4dff1d7065185810de3
bagus nur ali hidayat <bagooshidayat@yahoo.com>
2022-07-19 22:55:48 +0700

    create console interface and do the function math for getting average number

--- src/id/guusbajoos/main/MainApplication.java
+++ src/id/guusbajoos/main/MainApplication.java
@@ -2,11 +2,44 @@ package id.guusbajoos.main;
 
 import id.guusbajoos.util.GeneralUtility;
 
+import java.util.Scanner;
+
 public class MainApplication {
-    public static GeneralUtility generalUtility = new GeneralUtility();
+    // PROBLEM :
+    //   The witch will only leave if there is a brave programmer in the villager who can create a program to solve this problem :
+    //   If given two people whose age of death and year of death are known, find the average number of people the witch killed on year of birth of those people
+
+    public static final String EXIT_COMMAND = "exit";
+
 
     public static void main(String[] args) {
-        Integer deadVillagers = generalUtility.countDeadVillagers(5);
-        System.out.println("deadVillagers = " + deadVillagers);
+        // construct new utility class
+        GeneralUtility generalUtility = new GeneralUtility();
+
+        // open the scanner to detect user input
+        // input : person A age of death and year of death, person B age of death and year of death
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input first person's Age of Death : ");
+        Integer firstPersonAgeOfDeath = scanner.nextInt();
+        System.out.print("Input first person's Year of Death : ");
+        Integer firstPersonYearOfDeath = scanner.nextInt();
+        Integer firstPersonYearOfBorn = firstPersonYearOfDeath - firstPersonAgeOfDeath;
+        Integer diedVillagersFromFirstPerson = generalUtility.countDeadVillagers(firstPersonYearOfBorn);
+
+        System.out.print("Input second person's Age of Death : ");
+        Integer secondPersonAgeOfDeath = scanner.nextInt();
+        System.out.print("Input second person's Year of Death : ");
+        Integer secondPersonYearOfDeath = scanner.nextInt();
+        Integer secondPersonYearOfBorn = secondPersonYearOfDeath - secondPersonAgeOfDeath;
+        Integer diedVillagersFromSecondPerson = generalUtility.countDeadVillagers(secondPersonYearOfBorn);
+
+        System.out.println("First person died on year " + firstPersonYearOfDeath + " in age " + firstPersonAgeOfDeath + " years old. So the first person is born on year " + firstPersonYearOfBorn);
+        System.out.println("On year " + firstPersonYearOfBorn + ", " + diedVillagersFromFirstPerson + " has been killed by witch.");
+        System.out.println("Second person died on year " + secondPersonYearOfDeath + " in age " + secondPersonAgeOfDeath + " years old. So the second person is born on year " + secondPersonYearOfBorn);
+        System.out.println("On year " + secondPersonYearOfBorn + ", " + diedVillagersFromSecondPerson + " has been killed by witch.");
+
+        // find the average number of people the witch killed on year of birth of those people
+        System.out.println("Average number as magic incantation to make the witch leave = " + generalUtility.getAverageNumber(diedVillagersFromFirstPerson, diedVillagersFromSecondPerson));
     }
 }
--- src/id/guusbajoos/util/GeneralUtility.java
+++ src/id/guusbajoos/util/GeneralUtility.java
@@ -11,22 +11,45 @@ public class GeneralUtility {
     //   On the 4th year she kills 1 + 1 + 2 + 3 = 7 villagers
     //   On the 5th year she kills 1 + 1 + 2 + 3 + 5 = 12 villagers
     //   And so on...
-    //
+
     // ANALYSIS :
     //   this rule is counted based on fibonacci rules, which died villagers is counted by sum of n-th integer in fibonacci array (assume 0 / first number is not in fibonacci array)
+    //   param :
+    //     year : inputted integer to represent year which year witch did her nasty work
+    //   output :
+    //     deadVillagers : how many died villagers based on inputted integer year
     public Integer countDeadVillagers(Integer year) {
+        // define some variables that will be used in this function
         Integer deadVillagers = 1;
         Integer tempNum1 = 0, tempNum2 = 1, tempSum = 1;
 
+        // looping this calculation until reach intended year
         for (int x = 1; x < year; x++) {
+            // get fibonacci number per looping
             tempSum = tempNum1 + tempNum2;
             tempNum1 = tempNum2;
             tempNum2 = tempSum;
 
+            // increase number of deadVillagers by desired fibonacci number as looping continue
             deadVillagers = deadVillagers + tempSum;
-            System.out.println("dead villagers on year " + x + " : " + deadVillagers + " villagers.");
+            // System.out.println("dead villagers on year " + x + " : " + deadVillagers + " villagers.");
         }
 
         return deadVillagers;
+    }
+
+    // this function is intended to get the average number of people the witch killed on year of birth of those people
+
+    // ANALYSIS :
+    // first number of dead villagers will be added with second number of dead villagers, and it will divided by 2 to get average number
+    // this average number will be used as magic incantation by villager to make the witch leave the village
+    //   param :
+    //     firstNum : first inputted integer to represent how many villagers has been killed by the witch
+    //     secondNum : second inputted integer to represent how many villagers has been killed by the witch
+    //   output :
+    //     year : how many died villagers based on inputted integer year
+    public Double getAverageNumber(Integer firstNum, Integer secondNum) {
+
+        return (double) (firstNum + secondNum) / 2;
     }
 }
